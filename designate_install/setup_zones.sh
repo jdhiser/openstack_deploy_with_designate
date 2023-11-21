0810a734b384d2ba2dd3aa33343d04df4dc16670
Jason Hiser <jdhiser@gmail.com>
2023-11-21 14:36:12 +0000

    updated domain name for shen

--- designate_install/setup_zones.sh
+++ designate_install/setup_zones.sh
@@ -7,17 +7,17 @@ echo "Configuring neutron-api"
 juju ssh neutron-api/0 "sudo sed -i 's/dns_domain_ports/dns_domain_ports,subnet_dns_publish_fixed_ip/' /etc/neutron/plugins/ml2/ml2_conf.ini ; sudo service neutron-server restart"
 sleep 30
 
-export designate_bind_ip=10.246.112.222
+export designate_bind_ip=10.246.118.192
 
 source ~/openstack-bundles/stable/openstack-base/openrc
-openstack zone create --email jdhiser@gmail.com mtx1.os.
-openstack network set --dns-domain mtx1.os. ext_net
+openstack zone create --email jdhiser@gmail.com shen.os.
+openstack network set --dns-domain shen.os. ext_net
 openstack subnet set --dns-publish-fixed-ip ext_subnet
 openstack server create --image jammy-amd64 --flavor m1.small   --nic net-id=ext_net --key-name jdhKeyPair_admin --security-group Allow_SSH_admin dns-test
 sleep 30s # long enough for the server to get an IP
-openstack recordset list mtx1.os.
-dig dns-test.mtx1.os @$designate_bind_ip
-dig mtx39.maas @$designate_bind_ip
+openstack recordset list shen.os.
+dig dns-test.shen.os @$designate_bind_ip
+dig shen44.maas @$designate_bind_ip
 
 
 
